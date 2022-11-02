eadce38ce59e24f3fa7c8c7c6bbb4ac895f08ef8
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-02 13:02:15 -0400

    got the main part to shrink. now to find a way to animate it, add the carousel and buttons

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 $(() => {
-const $menuButton = $('#menuButton')
+const $mainSection = $('.main-section')
+$mainSection.css("animation", "main-animation 0.5s ease")
+
+const menuShow = () => {
+    $mainSection.toggleClass('active');
+}
 
 
 
-
+    $('#menuButton').on('click', menuShow)
 })
+
+// const menuButton = document.querySelector("#menuButton")
+// const mainSection = document.querySelector(".main-section")
+
+// menuButton.addEventListener("click", () => {
+//     mainSection.classList.toggle("active")
+// })
